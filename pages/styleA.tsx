cb674ae71d871163987c2a3f464b49539d8ff981
drnachio <nacho@binpar.com>
2019-09-27 16:52:43 +0200

    fix

--- pages/styleA.tsx
+++ pages/styleA.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Head from 'next/head';
+import dynamic from 'next/dynamic';
+import {resetBoard} from '../model/board';
 
+const Board = dynamic(import('../components/Board'), { ssr: false });
 export default (): JSX.Element => (
   <React.Fragment>
     <Head>
       <title>Buscaminas</title>
       <link rel="stylesheet" type="text/css" href="/static/style-a/style.css" />
     </Head>
-    <a href="/styleA" className="menu">Versión A</a>
-    <a href="/styleB" className="menu">Versión B</a>
-    
+    <button onClick={resetBoard} type="button" className="reset">Reiniciar</button>
+    <Board />
   </React.Fragment>
 );
