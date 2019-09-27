2a225b0fff375cb5325b93dd4163283f3d6bb1f9
drnachio <nacho@binpar.com>
2019-09-27 16:33:21 +0200

    Reset

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import dynamic from 'next/dynamic';
+import {resetBoard} from '../model/board';
 
 const Board = dynamic(import('../components/Board'), { ssr: false });
 export default (): JSX.Element => (
@@ -9,6 +10,7 @@ export default (): JSX.Element => (
       <title>Buscaminas</title>
       <link rel="stylesheet" type="text/css" href="/static/style.css" />
     </Head>
+    <button onClick={resetBoard} type="button" className="reset">Resetear</button>
     <Board />
   </React.Fragment>
 );
