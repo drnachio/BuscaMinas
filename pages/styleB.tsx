62e1d7282f8f634320fe385fc69ccc779fa49e2d
drnachio <nacho@binpar.com>
2019-09-27 17:00:38 +0200

    fix

--- pages/styleB.tsx
+++ pages/styleB.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import dynamic from 'next/dynamic';
+import Link from 'next/link';
 import {resetBoard} from '../model/board';
 
 const Board = dynamic(import('../components/Board'), { ssr: false });
@@ -9,8 +10,11 @@ export default (): JSX.Element => (
     <Head>
       <title>Buscaminas</title>
       <link rel="stylesheet" type="text/css" href="/static/style-b/style.css" />
-    </Head>
+    </Head>    
     <button onClick={resetBoard} type="button" className="reset">Reiniciar</button>
+    <Link href="/">
+      <button type="button" className="back">Regresar</button>
+    </Link>
     <Board />
   </React.Fragment>
 );
